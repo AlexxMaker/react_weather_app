86168cc1fb78e9ef8a080710ca88ec937936bf46
Alex Maker <Alexeus92@mail.ru>
2018-07-23 20:51:50 +0300

    Styles added

--- src/WeatherDisplay/WeatherDisplay.js
+++ src/WeatherDisplay/WeatherDisplay.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import "./WeatherDisplay.css";
+import {PageHeader} from 'react-bootstrap'
 
 class WeatherDisplay extends Component {
   state = {
@@ -9,7 +11,7 @@ class WeatherDisplay extends Component {
         const zip = this.props.zip;
         const URL = "http://api.openweathermap.org/data/2.5/weather?id=" +
           zip +
-          "&appid=e798d8a91519874ea95daee612e9a0cd&units=metric";
+          "&appid=45f336bc325b0b64d6431d7166aa1df6&units=metric";
         fetch(URL).then(res => res.json()).then(json => {
           this.setState({ weatherData: json });
         });
@@ -18,17 +20,41 @@ class WeatherDisplay extends Component {
         if (!weatherData) return <div>Loading...</div>;
         const weather = weatherData.weather[0];
         const iconUrl = "http://openweathermap.org/img/w/" + weather.icon + ".png";
+        const currentTemp = Math.round(weatherData.main.temp);
         return (
-          <div>
-            <h1>
-              {weather.main} in {weatherData.name}, {weatherData.sys.country}
-              <img src={iconUrl} alt={weatherData.description} />
-            </h1>
-            <p>Current: {weatherData.main.temp}°</p>
-            <p>High: {weatherData.main.temp_max}°</p>
-            <p>Low: {weatherData.main.temp_min}°</p>
-            <p>Wind Speed: {weatherData.wind.speed} km/hr</p>
-            <p>Pressure: {weatherData.main.pressure} hPa</p>
+          <div className="weather-wrapper">
+            <div className="weather-data">
+              <div className="weather-header">
+                <PageHeader>
+                  
+                    <h1>{weatherData.name} - {weatherData.sys.country}</h1>
+                  
+                </PageHeader>
+            </div>
+
+            <div>
+              <div className="weather-current">
+              <div className="left">
+                <img className="icon" src={iconUrl} alt={weatherData.description} />
+              </div>
+              
+              <div className="right">
+                <h1 className="temp">{currentTemp}C°</h1>
+              </div>
+              <div className="main">
+                <h2>{weather.main}</h2>
+              </div> 
+              </div>
+
+              <div className="weather-numbers">
+                <p>High: {weatherData.main.temp_max}°</p>
+                <p>Low: {weatherData.main.temp_min}°</p>
+                <p>Wind Speed: {weatherData.wind.speed} km/hr</p>
+                <p>Pressure: {weatherData.main.pressure} hPa</p>
+              </div>
+
+            </div>
+            </div>
           </div>
     );
         
