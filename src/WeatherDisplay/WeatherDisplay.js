af606e03f534067650c82ac05adeefc620f95c7a
AlexxMaker <alexeus92@mail.ru>
2018-07-23 09:38:44 +0300

    1

--- src/WeatherDisplay/WeatherDisplay.js
+++ src/WeatherDisplay/WeatherDisplay.js
@@ -5,11 +5,10 @@ constructor() {
     super();
     this.state = {
         weatherData: null
-    }
+    };
 }
 
     render() {
-
         const zip = this.props.zip;
         const URL = "http://api.openweathermap.org/data/2.5/weather?id=" +
           zip +
