6615b461ae8701e6505284838c729d2b553e6a8e
Alex Maker <Alexeus92@mail.ru>
2018-08-01 23:59:18 +0300

    Hardcoded, font changed

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import {Row, PageHeader} from 'react-bootstrap';
+import './Header.css'
 
 const Header = () => {
     return (
         <Row>
-            <PageHeader bsClass='page-header'>
-                <h1>Check the current weather</h1>
+            <PageHeader bsClass='page-header' className='header'>
+                <h1>Check the current weather 1234567890</h1>
             </PageHeader>
         </Row>
     );
--- src/WeatherDisplay/WeatherDisplay.js
+++ src/WeatherDisplay/WeatherDisplay.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import "./WeatherDisplay.css";
 import {Well, Grid, Row, Col} from 'react-bootstrap'
+import rain from './rain.png'
 
 class WeatherDisplay extends Component {
   state = {
@@ -8,55 +9,92 @@ class WeatherDisplay extends Component {
       }
 
     render() {
-        const zip = this.props.zip;
-        const URL = "http://api.openweathermap.org/data/2.5/weather?id=" +
-          zip +
-          "&appid=45f336bc325b0b64d6431d7166aa1df6&units=metric";
-        fetch(URL).then(res => res.json()).then(json => {
-          this.setState({ weatherData: json });
-        });
+        // const zip = this.props.zip;
+        // const URL = "http://api.openweathermap.org/data/2.5/weather?id=" +
+        //   zip +
+        //   "&appid=45f336bc325b0b64d6431d7166aa1df6&units=metric";
+        // fetch(URL).then(res => res.json()).then(json => {
+        //   this.setState({ weatherData: json });
+        // });
         
-        const weatherData = this.state.weatherData;
-        if (!weatherData) return <div>Loading...</div>;
-        const weather = weatherData.weather[0];
-        const iconUrl = "http://openweathermap.org/img/w/" + weather.icon + ".png";
-        const currentTemp = Math.round(weatherData.main.temp);
-        return (
-          <Grid className="weather-data">
-            <Row>
-              <Col md={12}>
-                <Well bsSize="small">
-                    <h2>{weatherData.name} - {weatherData.sys.country}</h2>
-                </Well>
-                </Col>
-            </Row>
+        // const weatherData = this.state.weatherData;
+        // if (!weatherData) return <div>Loading...</div>;
+        // const weather = weatherData.weather[0];
+        // const iconUrl = "http://openweathermap.org/img/w/" + weather.icon + ".png";
+        // const currentTemp = Math.round(weatherData.main.temp);
+    //     return (
+    //       <Grid className="weather-data">
+    //         <Row>
+    //           <Col md={12}>
+    //             <Well bsSize="small">
+    //                 <h2>{weatherData.name} - {weatherData.sys.country}</h2>
+    //             </Well>
+    //             </Col>
+    //         </Row>
 
-            <div>
-              <Row>
-            <Col md={6} sm={6} xs={12}>
-            <Well>
-              <div className="weather-current">
-                <img className="icon" src={iconUrl} alt={weatherData.description} />
-                <p className="temp"><span className="orange">{currentTemp}</span>°C</p>
-                <h1>{weather.main}</h1>
-              </div>
-              </Well>
-              </Col>
+    //         <div>
+    //           <Row>
+    //         <Col md={6} sm={6} xs={12}>
+    //         <Well>
+    //           <div className="weather-current">
+    //             <img className="icon" src={iconUrl} alt={weatherData.description} />
+    //             <p className="temp"><span className="orange">{currentTemp}</span>°C</p>
+    //             <h1>{weather.main}</h1>
+    //           </div>
+    //           </Well>
+    //           </Col>
               
-              <Col md={6} sm={6} xs={12}>
+    //           <Col md={6} sm={6} xs={12}>
               
-              <div className="weather-numbers">
-                <p>High: {weatherData.main.temp_max}°C</p>
-                <p>Low: {weatherData.main.temp_min}°C</p>
-                <p>Wind Speed: {weatherData.wind.speed} km/hr</p>
-                <p>Pressure: {weatherData.main.pressure} hPa</p>
-              </div>
+    //           <div className="weather-numbers">
+    //             <p>High: {weatherData.main.temp_max}°C</p>
+    //             <p>Low: {weatherData.main.temp_min}°C</p>
+    //             <p>Wind Speed: {weatherData.wind.speed} km/hr</p>
+    //             <p>Pressure: {weatherData.main.pressure} hPa</p>
+    //           </div>
             
-              </Col>
-              </Row>
-            </div>
-          </Grid>
-    );
+    //           </Col>
+    //           </Row>
+    //         </div>
+    //       </Grid>
+    // );
+
+    return (
+      <Grid className="weather-data">
+        <Row>
+          <Col md={12}>
+            <Well bsSize="small">
+                <h2>Weather in Kyiv - UA</h2>
+            </Well>
+            </Col>
+        </Row>
+
+        <div>
+          <Row>
+        <Col md={6} sm={6} xs={12}>
+        <Well>
+          <div className="weather-current">
+            <img className="icon" src={rain} alt='rain' />
+            <p className="temp"><span className="orange">27</span>°C</p>
+            <h1>Heavy Rain</h1>
+          </div>
+          </Well>
+          </Col>
+          
+          <Col md={6} sm={6} xs={12}>
+          
+          <div className="weather-numbers">
+            <p>High: 29°C</p>
+            <p>Low: 22°C</p>
+            <p>Wind Speed: 3 km/hr</p>
+            <p>Pressure: 1008 hPa</p>
+          </div>
+        
+          </Col>
+          </Row>
+        </div>
+      </Grid>
+);
         
     }
 }
